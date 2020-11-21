551b29b601b4d315cd877726d99f76aa22dfe55f
VadyChel <zyzel19@gmail.com>
2020-11-21 19:42:00 +0200

    Re-work API and add database

--- app.py
+++ app.py
@@ -1,81 +1,93 @@
-import json
-
+import uuid
+import hashlib
+import re
+from database import Database
 from sanic import response
 from sanic import Sanic
-from sanic_session import Session
-from api import bp as API_v1
 
 app = Sanic(__name__)
-app.register_blueprint(API_v1)
-session = Session(app)
+
+messages = {
+	"error_valid_json": {
+		"message": {
+			"error": "There is a valid json data"
+		}
+	},
+	"error_no_user": {
+		"message": {
+			"error": "There is no such user with such an ID"
+		}
+	},
+	"error_valid_password": {
+		"message": {
+			"error": "There is a valid password"
+		}
+	},
+	"error_mail_already_in_use": {
+		"message": {
+			"error": "Specified mail already in use"
+		}
+	},
+	"error_valid_email": {
+		"message": {
+			"error": "There is a valid email"
+		}
+	},
+	"succefly_create_user": {
+		"message": {
+			'succefly': 'Creating user was succefly'
+		}
+	}
+}
+
+@app.listener('before_server_start')
+async def setup_db(app, loop):
+    await Database().prepare_to_start()
 
 @app.get('/users')
 async def get_users(request):
-    users = {}
-    if request.ctx.session['users']:
-        for key, value in request.ctx.session['users'].items():
-            users.update({
-                value['name']:{
-                    'id': key, 
-                    'e-mail': value['e-mail'], 
-                    'pass-hash': value['pass-hash']
-                }
-            })
-        return response.json(users)
-    else:
-        return response.text('Users has not been created yet')
-
+	data = await Database().get_users()
+	return response.json(data)
 
 @app.get('/users/<id>')
 async def get_user(request, id):
-    if request.ctx.session.get('users'):
-        return response.json(request.ctx.session.get('users')[id])
-    else:
-        return response.text('There is no such user')
-
+	data = await Database().get_user(user_id=id)
+	if not data:
+		return response.json(messages["error_no_user"])
+	return response.json(data)
 
 @app.post('/users')
 async def create_user(request):
-    if request.ctx.session.get('users'):
-        if request.json:
-            request.ctx.session.get('users').update(request.json)
-            return response.json({
-                'message': {'succefly':
-                    'Creating user was sucefly'
-                }
-            })
-        else:
-            return response.json({
-                'message': {'error':
-                    'There is a valid json data'
-                }
-            })
-    else:
-        if request.json:
-            users = {}
-            users.update(request.json)
-            request.ctx.session['users'] = users
-            return response.json({
-                'message': {'succefly':
-                    'Creating user was sucefly'
-                }
-            })
-        else:
-            return response.json({
-                'message': {'error':
-                    'There is a valid json data'
-                }
-            })
+	fields = ["password", "name", "email"]
+	if request.json is not None:
+		if sorted(request.json.keys()) == sorted(fields):
+			template = re.compile("(^|\s)[-a-z0-9_.]+@([-a-z0-9]+\.)+[a-z]{2,6}(\s|$)")
+			if template.match(request.json.get("email")):
+				status = await Database().create_user(
+					user_id=str(uuid.uuid4()),
+					password=request.json.get("password"),
+					user_name=request.json.get("name"),
+					user_email=request.json.get("email")
+				)
+				if not status:
+					return response.json(messages["error_mail_already_in_use"])
+				return response.json(messages["succefly_create_user"])
+			else:
+				return response.json(messages["error_valid_email"])
+		else:
+			return response.json(messages["error_valid_json"])
+	else:
+		return response.json(messages["error_valid_json"])
 
 @app.delete('/users/<id>')
 async def delete_user(request, id):
-    pass
+	pass
 
 
 @app.patch('/users/<id>')
 async def edit_user(request, id):
-    pass
+	pass
 
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=5000, debug=True)
+	app.run(host="127.0.0.1", port=5000, debug=True)
---
+++ database.py
@@ -0,0 +1,73 @@
+import typing
+import aiosqlite
+import hashlib
+
+class Database:
+	async def prepare_to_start(self) -> None:
+		database = await aiosqlite.connect("database.db")
+		cursor = await database.execute("""CREATE TABLE IF NOT EXISTS users (
+			user_id TEXT,
+			pass_hash TEXT,
+			name TEXT,
+			e_mail TEXT
+		)""")
+		await database.commit()
+		await cursor.close()
+
+	async def get_users(self) -> list:
+		database = await aiosqlite.connect("database.db")
+		cursor = await database.execute("""SELECT * FROM users""")
+		data = await cursor.fetchall()
+		await cursor.close()
+		await database.close()
+
+		return [{
+			"id": user[0],
+			"name": user[2],
+			"email": user[3]
+		} for user in data]
+
+	async def get_user(self, user_id:str) -> typing.Union[tuple, bool]:
+		try:
+			database = await aiosqlite.connect("database.db")
+			cursor = await database.execute(f"""SELECT * FROM users WHERE user_id = "{user_id}" """)
+			data = await cursor.fetchone()
+			await cursor.close()
+			await database.close()
+
+			return {
+				"id": data[0],
+				"pass_hash": data[1],
+				"name": data[2],
+				"email": data[3]
+			}
+		except:
+			return False
+
+	async def create_user(
+		self, 
+		user_id:str, 
+		password:str, 
+		user_name:str, 
+		user_email:str
+	) -> typing.Union[bool, None]:
+		if user_email in [user["email"] for user in await self.get_users()]:
+			return False
+
+		database = await aiosqlite.connect("database.db")
+		pass_hash = hashlib.md5(password.encode()).hexdigest()
+		await database.execute(f"""INSERT INTO users (user_id, pass_hash, name, e_mail) VALUES (
+			"{user_id}",
+			"{pass_hash}",
+			"{user_name}",
+			"{user_email}"
+		)""")
+		await database.commit()
+		await database.close()
+		return True
+
+	async def delete_user(self):
+		pass
+
+	async def edit_user(self):
+		pass
